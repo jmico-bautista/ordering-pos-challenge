11000f6d8e997fe80f558518f11e90f19076a05c
Mico <utalk.johnmicoab@gmail.com>
2021-03-07 20:51:09 +0800

    baseurl

--- app/Config/App.php
+++ app/Config/App.php
@@ -24,7 +24,7 @@ class App extends BaseConfig
 	 * @var string
 	 */
 	// public $baseURL = 'http://localhost/posbang-proj/public/';
-	public $baseURL = 'https://ordering-sys-challenge.herokuapp.com/public/';
+	public $baseURL = 'https://ordering-sys-challenge.herokuapp.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
